1738c1157b8cb784f019a71443ec62bd7a589401
gigachad3011 <khantnyeinzaw6@gmail.com>
2022-12-05 04:45:15 +0630

    db setup complete

---
+++ q-and-a-app-api-laravel/app/Models/Answer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Answers extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['answer', 'question_id', 'user_id', 'is_solved'];
+}
